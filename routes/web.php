78204ef087e85dcd86ad97adac674ba90cd0c669
Joshua Welford <josh@welford.me>
2021-07-27 08:37:59 -0400

    WIP

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ $router->group(['prefix' => '/v1'], function () use ($router) {
     });
 
     $router->post('/playlists', function (\Illuminate\Http\Request  $request) {
+        header('Access-Control-Allow-Origin: *');
+        
         $data = $request->json()->all();
         $rules = ['tracks' => 'present|array', 'videos' => 'present|array'];
 
