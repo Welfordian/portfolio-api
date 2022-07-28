84959c41f82473986730c3f92beeec18c6a8a4ed
Joshua Welford <Josh@fish.cat>
2022-07-28 08:02:16 -0400

    WIP

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->group(['prefix' => '/v1'], function () use ($router) {
     $router->get('/bookmarks', function (\Illuminate\Http\Request $request) {
         $bookmarks = json_decode(file_get_contents(__DIR__ . '/../public/bookmarks.json'), true);
